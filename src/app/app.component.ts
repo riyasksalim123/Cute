5ae06830ae9a8c082fee1c2380acd76969f82118
riyas <riyasksalim@hotmail.com>
2016-10-21 21:45:49 +0530

    com

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,32 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
-import { StatusBar } from 'ionic-native';
+import { StatusBar ,BackgroundMode} from 'ionic-native';
 import { MyData } from '../pages/tabs/my-data'
 import { LoginPage} from '../pages/login/login'
 import { SearchCityService } from '../pages/searchcity/searchcityservice'
 import {CommonServices} from "../providers/common-services";
 
+declare var cordova:any;
+
 @Component({
   template: `<ion-nav  [root]="rootPage"></ion-nav>`,
-  providers:[MyData,SearchCityService,CommonServices]
+  providers:[MyData,SearchCityService,CommonServices,BackgroundMode]
 })
 export class MyApp {
  // rootPage = TabsPage;
   rootPage=LoginPage;
 
   constructor(platform: Platform) {
+
     platform.ready().then(() => {
+
+
      // alert("platform ready");
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
+
+
     });
   }
 }
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,23 +1,98 @@
-import { Component } from '@angular/core';
+import { Component,Injectable, NgZone } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {AlertController,ToastController } from 'ionic-angular';
+import {BackgroundMode ,Geolocation, Geoposition, BackgroundGeolocation} from 'ionic-native';
+import { Platform } from 'ionic-angular';
+import 'rxjs/add/operator/filter';
+@Injectable()
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController,private alertCtrl: AlertController,private toastCtrl: ToastController)
+  constructor(public navCtrl: NavController,
+              private alertCtrl: AlertController,
+              private toastCtrl: ToastController,
+              public back:BackgroundMode,
+              public platform:Platform,
+              public zone: NgZone)
   {
-   // this.prompt();
-    this.toaster();
-    setInterval(function(){
-
-
-     }, 3000);
+     this.prompt();
+     this.toaster();
+     this.startTracking();
 
   }
 
+  public watch: any;
+  public lat: number = 0;
+  public lng: number = 0;
+
+
+
+  startTracking() {
+    alert('BackgroundGeolocation:');
+    let config = {
+      desiredAccuracy: 0,
+      stationaryRadius: 20,
+      distanceFilter: 10,
+      debug: true,
+      interval: 2000
+    };
+
+    BackgroundGeolocation.configure((location) => {
+
+      console.log('BackgroundGeolocation:  ' + location.latitude + ',' + location.longitude);
+      alert('BackgroundGeolocation:  ' + location.latitude + ',' + location.longitude);
+      // Run update inside of Angular's zone
+      this.zone.run(() => {
+        this.lat = location.latitude;
+        this.lng = location.longitude;
+      });
+
+    }, (err) => {
+
+      console.log(err);
+      alert(JSON.stringify(err));
+
+    }, config);
+
+    // Turn ON the background-geolocation system.
+    BackgroundGeolocation.start();
+
+
+    // Foreground Tracking
+
+    let options = {
+      frequency: 3000,
+      enableHighAccuracy: true
+    };
+
+    this.watch = Geolocation.watchPosition(options).filter((p: any) => p.code === undefined).subscribe((position: Geoposition) => {
+
+      console.log(position);
+      alert(JSON.stringify(position));
+      // Run update inside of Angular's zone
+      this.zone.run(() => {
+        this.lat = position.coords.latitude;
+        this.lng = position.coords.longitude;
+
+      });
+
+    });
+  }
+
+  stopTracking() {
+    console.log('stopTracking');
+      alert('stopTracking');
+    BackgroundGeolocation.finish();
+    this.watch.unsubscribe();
+
+  }
+
+  public tester(){
+ // this.stopTracking();
+}
   toaster(){
     let toast = this.toastCtrl.create({
       message: 'User was added successfully',
@@ -32,23 +107,17 @@ export class LoginPage {
     toast.present();
   };
 
-  ionViewDidLoad() {
-    console.log('Hello Login Page');
-
-
-  }
-
 
     pos:number;
 
   getPosition()
   {
-    navigator.geolocation.getCurrentPosition((position) => {
-
-      this.pos = position.coords.latitude;
-      alert(this.pos);
-
-    })
+    // navigator.geolocation.getCurrentPosition((position) => {
+    //
+    //   this.pos = position.coords.latitude;
+    //   alert(this.pos);
+    //
+    // })
   };
 
 
